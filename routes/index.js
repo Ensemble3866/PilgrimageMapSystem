9534df2f29031781e7708d28b1e750bc515d02db
Ensemble3866 <sunny9113@gmail.com>
2017-05-31 14:46:14 +0800

    改版功能中途，完成新的景點格式

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ require('../lib/db');
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose'); 
-var Works = mongoose.model('works');
-var Scenes = mongoose.model('scenes');
 var Placemarks = mongoose.model('placemarks');
+var tagHistory = mongoose.model('tagHistory');
+var Scenes = mongoose.model('scenes');
 var Users = mongoose.model('users');
 var extArticles = mongoose.model('extArticles');
 
 /* Handle request of homepage. */
 router.get('/', function(req, res, next) {
 	
-	Placemarks.find({}).populate('work').exec(function(err, _placemark){
+	Placemarks.find({}).exec(function(err, _placemark){
 		if(err) res.status(500).send(err);
 		
 		req.session.auth = 3;
@@ -19,6 +19,7 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+/* Get user authority.  */
 router.post('/userAuth', function(req, res, next){
 	Users.findOne({ $and:[ { accountKey : req.body.userId }, { accountKind : req.body.website } ]}).exec(function(err, _user){
 		if(err) return handleError(err);
@@ -43,15 +44,125 @@ router.post('/userAuth', function(req, res, next){
 	});
 });
 
-/* Handle request of ajax. */
-router.get('/placemark/:placemarkId', function(req, res, next){
-	Placemarks.findOne({ _id: req.params.placemarkId }).populate('work').sort('work').exec(function(err, _placemark){
+/* Add new placemark. */
+router.post('/addPlacemark', function(req, res, next) {
+	var tagList = req.body.placemarkTagList.split(',');
+	var newPlacemark = new Placemarks({
+		name: req.body.placemarkName,
+		address: req.body.placemarkAddress,
+		latitude: req.body.placemarkLat,
+		longitude: req.body.placemarkLng,
+		description: req.body.placemarkDesc,
+		tag: tagList,
+		builder: req.session.curUser,
+		checker: req.session.curUser
+	});
+	//Add tag to tagHistory.
+	newPlacemark.save(function(err, newPlk){
 		if(err) res.status(500).send(err);
+		for(var i in tagList){
+			var newHistory = new tagHistory({
+				tag: tagList[i],
+				placemark:  newPlk._id,
+				isAdd: true,
+				builder: req.session.curUser,
+				checker: req.session.curUser
+			});
+			newHistory.save(function(err){
+				if(err) res.status(500).send(err);
+			});
+		}
+	});
+	res.redirect('/');
+});
 
-		Scenes.find({ placemark: _placemark._id }).sort('work').exec(function(err, _scenes){
-			res.render('placemarkInfo.ejs', {placemark: _placemark, scenes: _scenes});
+/* Get placemark infomation. */
+router.get('/placemark/:placemarkId', function(req, res, next){
+	Placemarks.findOne({ _id: req.params.placemarkId }).exec(function(err, _placemark){
+		if(err) res.status(500).send(err.message);
+		req.session.curPlacemark = _placemark._id;
+		res.render('placemarkInfo.ejs', {mod: req.session.auth, placemark: _placemark});
+	});
+});
+
+/*Delete placemark. */
+router.get('/delPlacemark', function(req, res, next) {
+	//確認權限
+	if(req.session.auth > 2){
+		res.redirect('/');
+		res.end();
+	}
+	Placemarks.findOne({ _id: req.session.curPlacemark }).exec(function(err, _placemark){
+		if(err) res.status(500).send(err.message);
+		var placemarkId = req.session.curPlacemark;
+		var tagList = _placemark.tag;
+		for(i = 0; i < tagList.length; i++){
+			var newHistory = new tagHistory({
+				tag: tagList[i],
+				placemark: placemarkId,
+				isAdd: false,
+				builder: req.session.curUser,
+				checker: req.session.curUser
+			});
+			newHistory.save(function(err){
+				if(err) res.status(500).send(err);
+			});
+		}
+		req.session.curPlacemark = null;
+		_placemark.remove(function(err){
+			if(err) res.status(500).send(err);
 		});
 	});
+	res.send("success");
+});
+
+/* Add current placemark's tag. */
+router.post('/submitTag', function(req, res, next){
+	Placemarks.findOne({ _id: req.session.curPlacemark }).exec(function(err, _placemark){
+		if(err) res.status(500).send(err);
+		_placemark.tag.push(req.body.tag);
+		_placemark.save(function(err){
+			if(err) res.status(500).send(err);
+		});
+		var newHistory = new tagHistory({
+			tag: req.body.tag,
+			placemark:  req.session.curPlacemark,
+			isAdd: true,
+			builder: req.session.curUser,
+			checker: req.session.curUser
+		});
+		newHistory.save(function(err){
+			if(err) res.status(500).send(err);
+		});
+	});
+	res.status(200).send("success");
+});
+
+/* Delete current placemark's tag. */
+router.post('/delTag', function(req, res, next){
+	Placemarks.findOne({ _id: req.session.curPlacemark }).exec(function(err, _placemark){
+		if(err) res.status(500).send(err);
+		for(var i in _placemark.tag){
+			if(_placemark.tag[i] == req.body.tag){
+				_placemark.tag.splice(i, 1);
+				_placemark.save(function(err){
+					if(err) res.status(500).send(err);
+				});
+				var newHistory = new tagHistory({
+					tag: req.body.tag,
+					placemark:  req.session.curPlacemark,
+					isAdd: false,
+					builder: req.session.curUser,
+					checker: req.session.curUser
+				});
+				newHistory.save(function(err){
+					if(err) res.status(500).send(err);
+				});
+				break;
+			}
+		}
+	});
+	res.status(200).send("success");
 });
 
 module.exports = router;
